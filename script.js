8dcf3b132fcbb67b2b8f7aa8c9476e4c95c9d97d
Grace Do <61570746+grace1501@users.noreply.github.com>
2023-11-21 16:12:27 -0500

    all checked

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ const n3 = 20;
 const n4 = 5;
 
 // Check if all above numbers are divisible by 5
-let ifAllIsDivisibleBy5 = (n1 % 5 === 0) && (n2 % 5 === 0) && (n3 % 5 === 5) && (n4 % 5 === 0);
-console.log(ifAllIsDivisibleBy5)
+let ifAllIsDivisibleBy5 = (n1 % 5 === 0) && (n2 % 5 === 0) && (n3 % 5 === 0) && (n4 % 5 === 0);
+console.log(`Are all the numbers divisible by 5? ${ifAllIsDivisibleBy5}`);
+
+let ifFirstNumLargerThanLastNum = n1 > n4;
+console.log(`Is the first number larger than the last number? ${ifFirstNumLargerThanLastNum}`);
+
+let arithmeticChain = (n2 - n1)*n3 % n4;
+console.log(`Arithmetric chain result: ${arithmeticChain}`)
 
 // Check one: add up to 50
 // This is a fairly simple operation using
@@ -22,7 +28,7 @@ const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
-const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+const isUnder25 = n1 < 25 && n2 < 25 && n3 < 25 && n4 < 25;
 
 // Check four: all unique numbers
 // This is long, and there are more efficient
@@ -34,10 +40,10 @@ const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
 // "isUnder25" as an alternative.
-const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
+const isValid = isSum50 && isTwoOdd && isUnder25 && isUnique;
 
 // Finally, log the results.
-console.log(isValid);
+console.log(`The four numbers are valid accroding to the criteria: ${isValid}`);
 
 // Here's another example of how this COULD be done,
 // but it SHOULD NOT be done this way. As programmers,
